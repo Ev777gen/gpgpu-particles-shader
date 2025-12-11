cce1fd86a1ea3431ee8d597341de3797c822d157
Ev777gen <fortheartist@mail.ru>
2025-12-11 08:42:30 +0500

    change base

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,7 @@
-// import { defineConfig } from 'vite'
-// import glsl from 'vite-plugin-glsl'
-
-// export default defineConfig({
-//   plugins: [
-//     glsl({
-//       include: /\.(glsl|wgsl|vert|frag|vs|fs)$/,
-//       compress: true,
-//     }),
-//   ],
-// })
-
 import glsl from 'vite-plugin-glsl';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
   plugins: [glsl()],
+  base: '/dist',
 });
